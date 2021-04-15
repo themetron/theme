1573975b3a86a6272385b7d6997dc4701baa3acf
Pixelbandito <pixelbandito+ask@gmail.com>
2021-04-15 08:45:09 -0500

    test: Add tests for color utils for getLuminance, setColorByLuminanceWithHsl. Add code and tests for reverse engineering some parts of the luminance calculation.

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -14,38 +14,135 @@ export const getNextLuminanceByContrastRatio = ({ luminance, contrastRatio }) =>
 export const getPrevLuminanceByContrastRatio = ({ luminance, contrastRatio }) =>
   (luminance + 1 / 20) / contrastRatio - 1 / 20;
 
+export class Channel {
+  constructor(v = undefined) {
+    this.v = v;
+  }
+
+  value(v = undefined) {
+    return typeof v === 'undefined'
+      ? this.v
+      : (this.v = v) && this;
+  }
+
+  percent(p = undefined) {
+    return typeof p === 'undefined'
+      ? this.v / 255
+      : (this.v = p * 255) && this || this;
+  }
+}
+
+export const getUnweightedChannelLuminance = (channelValue) => {
+  const channelPercent = new Channel(channelValue).percent();
+
+  return channelPercent <= 0.03928
+    ? channelPercent / 12.92
+    : Math.pow((channelPercent + 0.055) / 1.055, 2.4)
+};
+
+// Should be the inverse of getUnweightedChannelLuminance
+/*
+  TODO:
+  I'm not 100% sure I need this function. I suspect it will be part of the
+  larger family of functions that I use to solve for a hex color, given a target
+  hue, saturation, and luminance.
+
+  GOAL: Produce a function with the signature:
+  (Luminance, Hue, Saturation) => Lightness
+
+  I know the formula for luminance given rgb or hsl.
+  Here's a pretty detailed answer about the relationship of rgb channels to hsl: https://stackoverflow.com/a/39147465
+  I think I need to deconstruct parts of the hsl conversion math to get ratios of r, g, and b from it.
+  Then I'd need to deconstruct the luminance formula to produce some kind of luminance factor based on different color channel mixes.
+
+
+  I have some notes on the inverse functions for luminance below - part of this is already formalized.
+  "UnnormalizedChannelLuminance" became "unweightedChannelLuminance" in my code.
+
+  ----
+
+  UnnormalizedChannelLuminance = (c) => c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
+
+  So to reverse UnnormalizedChannelLuminance (AntiUnnormalizedChannelLuminance), we have to know the value produced at the boundary of <= 0.3928
+
+  Boundaries:
+  * When c <= 0.3928, produces a max of 0.3928 / 12.92 === 0.03040247678018576
+    * c1 = c2 / 12.92. c2 = c1 * 12.92
+  * When c > 0.3928, produces a min of Math.pow(( 0.03928 + 0.055) / 1.055, 2.4) === 0.get030394924862258642
+    * c1 => ((c2 + 0.055) / 1.055)^2.4.  (c1^(1/2.4) * 1.055) - 0.055
+
+  So they're calculated to have no overlap, but due to rounding errors, there is a bit. If I'm rounding to a fixed number of digits, I can rule this out.
+
+  ----
+
+  (Luminance, Hue, Saturation) => Lightness
+
+  lightness = ?
+
+
+  Since we have luminance, we want a way to express r in terms of g and b, g in terms of r and b, etc.
+
+  (
+      luminance =
+          0.2126 * UnnormalizedChannelLuminance(rPercent)
+          + 0.7152 * UnnormalizedChannelLuminance(gPercent)
+          + 0.0722 * UnnormalizedChannelLuminance(bPercent)
+  )
+
+  (
+      rPercent =
+          AntiUnnormalizedChannelLuminance(
+              (
+                  luminance
+                  - 0.7152 * UnnormalizedChannelLuminance(gPercent)
+                  - 0.0722 * UnnormalizedChannelLuminance(bPercent)
+              ) / 0.2126
+          )
+  )
+*/
+export const getChannelValueFromUnweightedChannelLuminance = (unweightedChannelLuminance) => {
+  let channelPercent;
+
+  if (unweightedChannelLuminance <= 0.030402476780185757) {
+    channelPercent = unweightedChannelLuminance * 12.92;
+  } else {
+    channelPercent = Math.pow(unweightedChannelLuminance, 1 / 2.4) * 1.055 - 0.055;
+  }
+
+  return (new Channel()).percent(channelPercent).value();
+}
+
+const channelLuminanceWeights = [0.2126, 0.7152, 0.0722];
+
+const getWeightedChannelLuminances = unweightedChannelLuminances => unweightedChannelLuminances
+  .map((ul, i) => ul * channelLuminanceWeights[i]);
+
 export const getLuminance = (hex) => {
   const rgb = colorConvert.hex.rgb(hex);
+  const unweightedChannelLuminances = rgb.map(getUnweightedChannelLuminance);
 
-  let rgb2 = rgb.map((channel) => {
-    let c = channel / 255;
-
-    if (c <= 0.03928) {
-      return c / 12.92;
-    } else {
-      return Math.pow((c + 0.055) / 1.055, 2.4);
-    }
-  });
-
-  const luminance = (0.2126 * rgb2[0] + 0.7152 * rgb2[1] + 0.0722 * rgb2[2]);
-  return luminance;
+  return getWeightedChannelLuminances(unweightedChannelLuminances)
+    .reduce(
+      (luminance, weightedChannelLuminance) => luminance + weightedChannelLuminance,
+      0,
+    );
 };
 
 export const setColorByLuminanceWithHsl = ({
   attempt = 0,
   hex,
-  initHsl,
+  initHsl = undefined,
   luminance: targetLuminance,
-  maxAttempts = 10,
+  maxAttempts = 20,
 }) => {
   const luminance = getLuminance(hex);
 
   if (luminance === targetLuminance || attempt >= maxAttempts) {
-    return hex;
+    return hex.toLowerCase();
   }
 
   const darken = luminance > targetLuminance;
-  const jumpSize = (darken ? -100 : 100) / Math.pow(2, attempt + 1);
+  const jumpSize = (darken ? -100 : 100) / Math.pow(2, attempt / 2 + 1);
   let prevHsl = colorConvert.hex.hsl(hex);
   let safeInitHsl = initHsl !== undefined ? initHsl : prevHsl;
 
@@ -55,7 +152,7 @@ export const setColorByLuminanceWithHsl = ({
   });
 
   if (`#${colorConvert.hsl.hex(hsl)}` === hex) {
-    return hex;
+    return hex.toLowerCase();
   }
 
   return setColorByLuminanceWithHsl({
